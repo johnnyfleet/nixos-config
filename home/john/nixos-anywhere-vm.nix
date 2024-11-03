ea16584ee853a38d44ae2564c237ac52b676a974
John Stephenson <johnnyfleet@gmail.com>
2024-11-04 10:39:44 +1300

    added state version for home-manager

--- home/john/nixos-anywhere-vm.nix
+++ home/john/nixos-anywhere-vm.nix
@@ -4,4 +4,17 @@
     #################### Required Configs ####################
     ./core/default.nix # required
   ];
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
