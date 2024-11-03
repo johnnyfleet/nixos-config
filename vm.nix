233b756c62c83af2b0ce9d3055434f9d0620e498
John Stephenson <johnnyfleet@gmail.com>
2024-11-03 22:28:14 +1300

    removed autologin

--- vm.nix
+++ vm.nix
@@ -109,7 +109,7 @@
   services.xserver.xkb.layout = "us";
   services.xserver.xkb.variant = "";
 
-  services.displayManager.autoLogin.user = "guest";
+  #services.displayManager.autoLogin.user = "guest";
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.desktopManager.xfce.enableScreensaver = false;
 
