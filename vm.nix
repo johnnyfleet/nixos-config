885a77fbf27f09657586dfa14c1beabc0060dcdc
John Stephenson <johnnyfleet@gmail.com>
2024-11-03 23:05:46 +1300

    testing pulling keys from github

--- vm.nix
+++ vm.nix
@@ -107,6 +107,7 @@
     home = "/home/guest";
     extraGroups = [ "wheel" ];
     initialPassword = "guest";
+    openssh.authorizedKeys.keys = [ builtins.readFile (builtins.fetchurl https://github.com/johnnyfleet.keys) ];
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
