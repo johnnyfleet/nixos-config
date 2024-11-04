5fcf4bb00fcb50187781c76509a1ad7859012f41
John Stephenson <johnnyfleet@gmail.com>
2024-11-04 17:56:31 +1300

    UPdated shae for github keys

--- hosts/common/users/john.nix
+++ hosts/common/users/john.nix
@@ -14,7 +14,7 @@
     openssh.authorizedKeys.keys = let
       authorizedKeys = pkgs.fetchurl {
         url = "https://github.com/johnnyfleet.keys";
-        sha256 = "766a3078616f0c600d487b1909adb28f70d49ce4ace3966aaaa5052924850720";
+        sha256 = "3d81dd9e8a6c1092757c3a687be6733837fa4f9d3853d8a5efd05dfa8a586138";
       };
     in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
   };
