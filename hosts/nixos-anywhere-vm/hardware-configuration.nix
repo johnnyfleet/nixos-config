9fa07620da453af325b0328e6ba2eaf59e015406
John Stephenson <johnnyfleet@gmail.com>
2024-12-23 09:44:10 +1300

    adding correct hardware config back in

--- hosts/nixos-anywhere-vm/hardware-configuration.nix
+++ hosts/nixos-anywhere-vm/hardware-configuration.nix
@@ -14,20 +14,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/52dd3e08-c7a4-4913-902c-c53c395c33ed";
+    { device = "/dev/disk/by-uuid/6315a703-7241-46d8-a025-7edf6ffd7749";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/430B-0137";
+    { device = "/dev/disk/by-uuid/3320-5757";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-    size = 2*1024;
-  } ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -35,6 +32,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s8.useDHCP = lib.mkDefault true;
+  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
