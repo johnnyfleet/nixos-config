1412977339865de97f34866ab8c42b2dd1c36746
John Stephenson <johnnyfleet@gmail.com>
2024-12-28 22:33:46 +1300

    added initial base packages

--- home/john/optional/regular-programs.nix
+++ home/john/optional/regular-programs.nix
@@ -3,9 +3,12 @@
 
   # Packages that should be installed to the user profile.
   home.packages = [
-    pkgs.filezilla
-    pkgs.easytag
-    pkgs.kdePackages.partitionmanager
+
+    # This aims to replicate what I had on john-laptop originally.
+
+    # Base Packages 
+    
+    
     pkgs.kdePackages.discover
     pkgs.picard
     pkgs.vlc
@@ -14,6 +17,17 @@
     pkgs.remmina
     pkgs.discord
     pkgs.flameshot
+
+    pkgs.ansible-core # Ansible, duh
+    pkgs.ansible-lint # Linting to check playbooks
+    pkgs.baobab #Disk Usage Analyser
+    pkgs.easytag # Edit MP3 ID3 files
+    pkgs.filezilla # FTP client
+    pkgs.gnome-keyring # Keyring to access secrets wallet - used for VS code
+    #pkgs.gparted # Disk Partition Manager
+    pkgs.kdePackages.partitionmanager # Disk Partition Manager
+    pkgs.guake # drop-down style terminal
+
 
   ];
 
