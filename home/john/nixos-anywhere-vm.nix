2073dae91bd153f783f1cef18ce5a2a1143e9f9d
John Stephenson <johnnyfleet@gmail.com>
2024-12-30 23:47:39 +1300

    refactro optional programs to dormant module.

--- home/john/nixos-anywhere-vm.nix
+++ home/john/nixos-anywhere-vm.nix
@@ -5,7 +5,8 @@
     ./core/default.nix # required
     ./optional/plasma-manager.nix # set up of plasma manager on plasma. 
     ./optional/regular-programs.nix # Additional programs I usually have installed
-    #./optional/gaming.nix # Additional programs I usually have installed
+    #./optional/additional-programs.nix # Additional programs I used to have but don't think I need right now. 
+    #./optional/gaming.nix # Heroic & Lutris
   ];
 
   # This value determines the home Manager release that your
--- home/john/optional/regular-programs.nix
+++ home/john/optional/regular-programs.nix
@@ -13,52 +13,30 @@
     kdePackages.ghostwriter        # Really good markdown editor, similar to Apostrophe
     spotify                        # Music baby. 
 
-    ansible # Ansible, duh
-    ansible-lint # Linting to check playbooks
     baobab #Disk Usage Analyser
     easytag # Edit MP3 ID3 files
-    filezilla # FTP client
     gnome-keyring # Keyring to access secrets wallet - used for VS code
-    #gparted # Disk Partition Manager
     kdePackages.partitionmanager # Disk Partition Manager
-    guake # drop-down style terminal
     jq                    # JSON parser
     ncdu                  # Disk usage CLI
     neofetch              # Nice system info CLI command
     fastfetch          # Neofetch replacement
     nettools             # ipconfig etc.
     packer                # create VM images
-    picard                # MusicBrainz - music info lookup
     remmina               # Remote Desktop viewer
-    retext                # Simple markdown editor
-    rpcbind               # Network and RPC program
     screen                # detached terminal session
-    scribus               # PDF editor (for cd covers)
-    terminator            # Improved shell
     vlc                   # VLC media player
 
   # ARCH_PACKAGES:
     #appimagelauncher      # Makes it easy to install appimage files.
-    arduino               # Arduino IDE
     aribb24               # Allows VLC to play recordings done via HDHomeRun
-    asciidoctor           # Convert asciidocs to different formats
     audacious             # Music player
-    #base-devel            # Fakeroot and other utils needed for AUr installation via Yay
     btop               # Advanced system utilisation monitor (think top, htop)
-    #bootsplash-systemd        # Startup splash screen
-    #bootsplash-theme-manjaro  # Startup splash screen - Manjaro
-    chezmoi
-    chromium
-    #crossover-extras      # Install crossover and dependencies that are required to install windows apps.
-    discord
     duf                   # Fancier version of df
     eza                   # alternative to ls which is more colourful and includes icons.
     du-dust                  # Filesystem cli tool
     firefox
     flameshot             # Screen Snipping tool
-    freerdp
-    #gestures              # To get multi finger gestures on touchpad working
-    gimp                  # Image editor.
     gh                # GitHub CLI tool
     glances          # Another monitoring tool
     guestfs-tools         # `virt-sparsify and other libvirt tools
@@ -66,21 +44,13 @@
     htop
     iucode-tool           # Check intel microcode for updates (dependency on needrestart)
     libinput-gestures      # To get multi finger gestures on touchpad working
-    #matray                # Manjaro Update tray notifier
     mosh                  # For connecting over SSH and maintaining session.
-    mutt                  # Email CLI client for reading root mail.
-    #needrestart           # Utility to check if a restart is needed post update.
     neovim                # Vim
     obsidian              # Note taking app
     ipafont           # extra fonts for unicode support. Used for Japanese characters.
-    pandoc                # Convert markdown and asciidoc
-    qmmp                  # winamp like music player
     sof-firmware          # Fixes sound on Lenovo x1 Carbon Gen 9.
     speedtest-cli         # Check internet speed from CLI1
-    #tlpui                 # UI for power management
     trash-cli             # Simple utility to clean trash but running trash-empty either as user or sudo for root.
-    #ttf-font-awesome      # extra fonts for unicode support. Used for icons.
-    unzip
     yubikey-manager       # To manage YubiKey on device.
     yubikey-manager-qt    # GUI to managhe Yubikey
     yubioath-flutter      # Desktop tool to manage OATH requests via Yubikey. This one doesn't work - replace with flatpak version.
@@ -95,7 +65,6 @@
     #- com.yubico.yubioath                         # Yubikey OATH tool
     #- com.plexamp.Plexamp                         # Plexamp player
     #- com.moonlight_stream.Moonlight              # Moonlight came streaming client
-    apostrophe           # Markdown editor - nice and clean.
     plexamp                         # Plexamp player
     
 
@@ -121,8 +90,6 @@
     #- xrdp                           # Allow RDP (via xvnc) fN client
     zoom-us                           # Video conferencing. AUR seems to work better than Flatpak.
     #- gamehub                       # Updated UI to manage games.
-    makemkv                       # MakeMKV - DVD/Blue-Ray to MKV formatter.
-    mqtt-explorer                 # Allows you to view MQTT messages
     slack
     quickemu                   # Quickly create and run vms. 
     
