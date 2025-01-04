7464b50a1e37fd973f4eef738c4313982f77fe3e
John Stephenson <johnnyfleet@gmail.com>
2025-01-05 11:51:23 +1300

    update suu alias to use hostname for flake

--- home/john/core/zsh/default.nix
+++ home/john/core/zsh/default.nix
@@ -11,7 +11,7 @@
 
     shellAliases = {
       ll = "eza -al --icons --color";
-      suu = "sudo nixos-rebuild switch --flake ~/.config/nixos-config#vm";
+      suu = "sudo nixos-rebuild switch --flake ~/.config/nixos-config";
       gp = "cd ~/.config/nixos-config && git pull";
       sgc = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
     };
