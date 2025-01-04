d07671b7ca644e8ef3092d3e9a445e3ff75ea828
John Stephenson <johnnyfleet@gmail.com>
2025-01-05 10:54:24 +1300

    switch laptop to grub (as old)

--- hosts/john-sony-laptop/default.nix
+++ hosts/john-sony-laptop/default.nix
@@ -46,12 +46,12 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
-  #boot.loader.grub.enable = true;
-  #boot.loader.grub.device = "/dev/vda";
-  #boot.loader.grub.useOSProber = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
 
   ############################ NETWORKING ########################
 
