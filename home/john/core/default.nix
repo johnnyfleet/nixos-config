f6b075b4e9b141261ab58c3fa9ae8b8092ef96b7
John Stephenson <johnnyfleet@gmail.com>
2025-01-05 16:03:40 +1300

    added default vscode extensions

--- home/john/core/default.nix
+++ home/john/core/default.nix
@@ -18,5 +18,14 @@
   # Enable VSCode
   programs.vscode = {
     enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      henriiik.vscode-sort
+      yzhang.markdown-all-in-one
+      github.copilot
+      github.copilot-chat
+      github.vscode-pull-request-github
+      eamodio.gitlens
+    ];
   };
 }
