78bb576665143ae877d094481987f16ffd6bde4b
John Stephenson <johnnyfleet@gmail.com>
2025-01-05 14:20:05 +1300

    commetn out virtualisation options on sony laptop

--- hosts/john-sony-laptop/default.nix
+++ hosts/john-sony-laptop/default.nix
@@ -93,30 +93,32 @@
 
   ########################## VIRTUALISATION ########################
 
-  # Options for the screen
-  virtualisation.vmVariant = {
-    virtualisation.resolution = {
-      x = 1280;
-      y = 1024;
+  /*
+    # Options for the screen
+    virtualisation.vmVariant = {
+      virtualisation.resolution = {
+        x = 1280;
+        y = 1024;
+      };
+      virtualisation.cores = 4;
+      virtualisation.memorySize = 2048;
+      virtualisation.qemu.options = [
+        # Better display option
+        "-vga virtio"
+        "-display gtk,zoom-to-fit=false,show-cursor=on"
+        # Enable copy/paste
+        # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
+        "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
+        "-device virtio-serial-pci"
+        "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
+      ];
     };
-    virtualisation.cores = 4;
-    virtualisation.memorySize = 2048;
-    virtualisation.qemu.options = [
-      # Better display option
-      "-vga virtio"
-      "-display gtk,zoom-to-fit=false,show-cursor=on"
-      # Enable copy/paste
-      # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
-      "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
-      "-device virtio-serial-pci"
-      "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
-    ];
-  };
 
-  # VM guest additions to improve host-guest interaction
-  #services.spice-vdagentd.enable = true;
-  #services.qemuGuest.enable = true;
-  #services.spice-autorandr.enable = true;
+    # VM guest additions to improve host-guest interaction
+    services.spice-vdagentd.enable = true;
+    services.qemuGuest.enable = true;
+    services.spice-autorandr.enable = true;
+  */
 
   #security.sudo.wheelNeedsPassword = false;
 
