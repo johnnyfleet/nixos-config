21c3f110096e4e3f9cf3531012cade8aa5737156
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-04-28 12:04:39 +1200

    Created work applications app module

---
+++ home/john/optional/work-applications.nix
@@ -0,0 +1,11 @@
+# This module adds programs I find useful when using the device for work. 
+
+{ pkgs, ... }:
+{
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    bluemail
+  ]
+
+}
