dc9322c1f0e733d5569418f973271707e2c7d091
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-04-30 18:23:14 +1200

    feat: enable localgame transfers on steam

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -3,8 +3,8 @@
   # Install Steam client and enable remote play.
   programs.steam = {
     enable = true;
-    remotePlay = {
-      openFirewall = true;
-    };
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers  };
   };
 }
