c6844fb658f3d2b331bd9705a9d659b83a6a06ec
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-05-08 09:47:26 +1200

    fix: Add printing firewall rules

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -7,4 +7,9 @@
   environment.systemPackages = with pkgs; [
      hplipWithPlugin # HP Printer utility
   ];
+
+  # Allow discovery using this port range. See: https://discourse.nixos.org/t/how-to-detect-what-firewall-ports-i-need-to-open-for-a-certain-service/37314/6
+  networking.firewall.allowedUDPPortRanges = [
+    { from = 49152; to = 65535; }
+  ];
 }
