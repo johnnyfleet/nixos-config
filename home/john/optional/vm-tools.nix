237f52b79c8a25ef877a32fb3565e43d50172194
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-06-25 11:55:34 +1200

    fix: Switch quickemu to build from git (to fix qemu 11 issues)

--- home/john/optional/vm-tools.nix
+++ home/john/optional/vm-tools.nix
@@ -5,7 +5,16 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     guestfs-tools # `virt-sparsify and other libvirt tools
-    quickemu # Quickly create and run vms.
+    # Build quickemu from git master to fix QEMU 10.0.0 compatibility issue
+    # See: https://github.com/quickemu-project/quickemu/pull/1640
+    (quickemu.overrideAttrs (oldAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "quickemu-project";
+        repo = "quickemu";
+        rev = "9f90d46ea195d62a8b84c6dd45ea5b1443bb3e46";
+        sha256 = "sha256-M5/EIzh19VlxQ5yTEvupCa5j/PvyqBx7/WqXJ9319oo=";
+      };
+    }))
   ];
 
   # Configure virt-manager to correctly connect to the libvirt daemon.
