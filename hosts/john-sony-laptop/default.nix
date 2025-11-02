2fa3fdf2534a3fcbc6849a506af04e8862c09135
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-11-02 16:22:41 +1300

    fix: updated john-sony grub/efi options

--- hosts/john-sony-laptop/default.nix
+++ hosts/john-sony-laptop/default.nix
@@ -72,10 +72,18 @@
   # sudo nixos-rebuild --install-bootloader boot
 
   # Grub Bootloader
-  boot.loader.systemd-boot.enable = false;
+
+  # Systemd-boot Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = false;
+  boot.loader.efi.efiSysMountPoint = "/efi";
+
+  # boot.loader.systemd-boot.enable = false;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.devices = [ "nodev" ];
+  
   #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
   #boot.loader.grub.efiSupport = true;
   #boot.loader.grub.useOSProber = true;
 
