4f22ab7d3b39a689c9a4e795d839dc2b9d58423f
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-11-25 22:41:17 +1300

    fix: added additional options for syncthing

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -1,8 +1,14 @@
 { config, pkgs, ... }:
 {
-  # Enable Syncthing
+  # Enable Syncthing. Access through http://127.0.0.1:8384/ 
+  # NOTE: Gui is not opened in firewall by default. If you do set the password below. 
   services.syncthing = {
     enable = true;
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+    # Optional: GUI credentials (can be set in the browser instead)
+    /* settings.gui = {
+      user = "myuser";
+      password = "mypassword";
+    }; */
   };
 }
