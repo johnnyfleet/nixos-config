f41a1076d047995a1ddb1c13ea69ccc31199b2a1
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2025-11-27 08:12:53 +1300

    fix: setup syncthing to run in local user folder

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -4,7 +4,10 @@
   # NOTE: Gui is not opened in firewall by default. If you do set the password below. 
   services.syncthing = {
     enable = true;
+    user = "john";
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+    dataDir = "/home/john/Pictures/Syncthing"; # Change this to your desired data directory
+    configDir = "/home/john/.config/syncthing";
     # Optional: GUI credentials (can be set in the browser instead)
     /* settings.gui = {
       user = "myuser";
