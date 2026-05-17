4690cc02ab85064a14fd305f97e4657f3f898f1a
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2026-05-18 09:36:01 +1200

    feat: removed virt-sparsify to free 4Gb.

--- home/john/optional/vm-tools.nix
+++ home/john/optional/vm-tools.nix
@@ -7,7 +7,9 @@
 }: {
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    guestfs-tools # `virt-sparsify and other libvirt tools
+    # NOTE: Removing as it pulls in 4Gb of data. If need to run virt sparsify run on demand with
+    # nix run nixpkgs#guestfs-tools
+    #guestfs-tools # `virt-sparsify and other libvirt tools.
     quickemu # QEMU 10.0.0 compatibility fix is now included in nixpkgs
   ];
 
