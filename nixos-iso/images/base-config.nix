5776c47edc2f8ad269f438ec91fcbec350b8a465
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2026-08-07 21:42:37 +1200

    chore: update ssh keys

--- nixos-iso/images/base-config.nix
+++ nixos-iso/images/base-config.nix
@@ -53,7 +53,7 @@
       openssh.authorizedKeys.keys = let
         authorizedKeys = pkgs.fetchurl {
           url = "https://github.com/johnnyfleet.keys";
-          sha256 = "fce5536148d8d8f607dc0612d47c9ca721a75c62539a07e571183f56070c31d1";
+          sha256 = "sha256-GKtaF7Y6CHPjpJ/61BDLeQ4TCvIaO6rrWDtc8gRHiL8=";
         };
       in
         pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
