84251edc494c4f6cdb3b5bda831130aa89c1aac2
John Stephenson <14134347+johnnyfleet@users.noreply.github.com>
2026-08-27 12:26:38 +1200

    feat: install markdown addon in thunderbird

--- home/john/optional/work-applications.nix
+++ home/john/optional/work-applications.nix
@@ -1,5 +1,26 @@
 # This module adds programs I find useful when using the device for work.
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # Markdown Here Revival is not in nixpkgs, so package the xpi into the layout
+  # home-manager's thunderbird module expects: the add-on's gecko id under
+  # share/mozilla/extensions/{ec8030f7-...}.
+  markdown-here-revival = pkgs.stdenvNoCC.mkDerivation {
+    pname = "thunderbird-markdown-here-revival";
+    version = "3.0.2";
+
+    src = pkgs.fetchurl {
+      url = "https://addons.thunderbird.net/thunderbird/downloads/file/1018457/markdown_here_revival-3.0.2-tb.xpi";
+      hash = "sha256-OM7QlEZoYPXYi7WCFh8/fKzn19gUa/sW0lN5HLB4CN8=";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      dir="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+      mkdir -p "$dir"
+      install -m444 "$src" "$dir/markdown-here-revival@xul.calypsoblue.org.xpi"
+    '';
+  };
+in {
   # Packages that should be installed to the user profile.
   # home.packages = with pkgs; [
   #   bluemail
@@ -14,6 +35,11 @@
         isDefault = true;
         # Optionally you can define settings or use a custom path here
         # path = "~/.thunderbird/default";
+        extensions = [markdown-here-revival];
+        settings = {
+          # Auto-enable Nix-installed add-ons instead of enabling them by hand.
+          "extensions.autoDisableScopes" = 0;
+        };
       };
     };
   };
